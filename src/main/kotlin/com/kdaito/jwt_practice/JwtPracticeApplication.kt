9366641c73275e12b0e2ff245f3ad3fe01d788c2
Kdaito <kdaito0620@gmail.com>
2024-07-06 18:29:22 +0900

    [Add] init project

---
+++ src/main/kotlin/com/kdaito/jwt_practice/JwtPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.kdaito.jwt_practice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JwtPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<JwtPracticeApplication>(*args)
+}
