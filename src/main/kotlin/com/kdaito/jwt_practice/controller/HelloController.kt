e9caa6cd539c40f1d3c6b0a4a672f1275675f861
Kdaito <kdaito0620@gmail.com>
2024-07-07 17:54:11 +0900

    [Add] Add authenticated user info in greeting response

--- src/main/kotlin/com/kdaito/jwt_practice/controller/HelloController.kt
+++ src/main/kotlin/com/kdaito/jwt_practice/controller/HelloController.kt
@@ -4,6 +4,8 @@ import com.kdaito.jwt_practice.dao.GreetingRequest
 import com.kdaito.jwt_practice.dao.GreetingResponse
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -12,7 +14,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class HelloController {
     @PostMapping("/greeting")
-    fun get(@RequestBody request: GreetingRequest):  ResponseEntity<GreetingResponse> {
-        return ResponseEntity(GreetingResponse(request.greeting + "," + "!"), HttpStatus.OK)
+    fun get(@RequestBody request: GreetingRequest, @AuthenticationPrincipal userDetails: UserDetails?):  ResponseEntity<GreetingResponse> {
+        if (userDetails != null) {
+            return ResponseEntity(GreetingResponse(request.greeting + "," + userDetails.username + "!"), HttpStatus.OK)
+        }
+        return ResponseEntity(GreetingResponse(request.greeting + "!"), HttpStatus.OK)
     }
 }
